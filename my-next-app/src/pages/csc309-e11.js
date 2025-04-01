0f9bd2d7d9186f73f20bc7ddaeea4b38dda3e37c
chomnk <86968977+chomnk@users.noreply.github.com>
2025-04-01 00:20:41 -0400

    init

---
+++ my-next-app/src/pages/csc309-e11.js
@@ -0,0 +1,9 @@
+export default function AutoTester() {
+  const utorid = 'panteng1';
+  return (
+    <div id="auto-tester">
+      Hi. I am {utorid}, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+}
